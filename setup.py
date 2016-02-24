ece6815361a07f2761e7921b620e28b46e620240
Marta Nabozny <marta.nabozny@cloudover.io>
2016-02-24 10:28:01 +0100

    Package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.command.install import install as _install
 
 setup(
   name = 'coredhcp',
-  packages = find_packages(exclude='config'),
+  packages = find_packages(exclude=['config']),
   version = '16.02.03',
   description = 'DHCP support for isolated networks in CoreCluster IaaS',
   author = 'Marta Nabozny',
